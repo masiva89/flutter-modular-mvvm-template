ba451fd6508c99956475a8359e1150c315d03763
Utku Aydoğdu <94013786+masiva89@users.noreply.github.com>
2024-03-21 17:44:07 +0300

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // You should use this returned widget instead of the active one.
+    /* return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        unFocusAll(context);
+      },
+      child: MaterialApp.router(
+        key: navigatorKey,
+        scaffoldMessengerKey: scaffoldMessengerKey,
+        routerConfig: _appRouter.config(),
+        builder: CustomResponsive.build,
+        theme: CustomDarkTheme().themeData,
+        darkTheme: CustomDarkTheme().themeData,
+        themeMode: context.watch<ProductViewModel>().state.themeMode,
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+      ),
+    ); */
     return MaterialApp(
       title: 'FLUTTER MODULAR MVVM TEMPLATE',
       theme: ThemeData(
@@ -53,26 +72,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // You should use this returned widget instead of the active one.
-    /* return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onTap: () {
-        unFocusAll(context);
-      },
-      child: MaterialApp.router(
-        key: navigatorKey,
-        scaffoldMessengerKey: scaffoldMessengerKey,
-        routerConfig: _appRouter.config(),
-        builder: CustomResponsive.build,
-        theme: CustomDarkTheme().themeData,
-        darkTheme: CustomDarkTheme().themeData,
-        themeMode: context.watch<ProductViewModel>().state.themeMode,
-        localizationsDelegates: context.localizationDelegates,
-        supportedLocales: context.supportedLocales,
-        locale: context.locale,
-      ),
-    ); */
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
