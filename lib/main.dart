4bf6666e7fd3860f957ce6a2646db005ec473d4b
Utku Aydoğdu <94013786+masiva89@users.noreply.github.com>
2024-03-21 17:13:59 +0300

    main Widget uğdate

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,26 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // You should use this returned widget instead of the active one.
+    /* return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        unFocusAll(context);
+      },
+      child: MaterialApp.router(
+        key: navigatorKey,
+        scaffoldMessengerKey: scaffoldMessengerKey,
+        routerConfig: _appRouter.config(),
+        builder: CustomResponsive.build,
+        theme: CustomDarkTheme().themeData,
+        darkTheme: CustomDarkTheme().themeData,
+        themeMode: context.watch<ProductViewModel>().state.themeMode,
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+      ),
+    ); */
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
